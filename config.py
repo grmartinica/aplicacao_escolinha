db2dc9ecd9de1b3f214a36746d471a1bdb02cb34
Pedro Santos <106214744+PedroSantos0806@users.noreply.github.com>
2025-11-11 12:37:21 -0300

    conectando com o banco de dados

--- config.py
+++ config.py
@@ -1,11 +1,24 @@
 import os
-
+from urllib.parse import quote_plus
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL"
-        "mysql+mysqlconnector://root:senha@localhost:3306/aplicacao_escolinha"
+    SECRET_KEY = os.getenv("SECRET_KEY", "projetomartinica")
+
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "Phlgbabi@10")
+    DB_HOST = os.getenv("DB_HOST", "191.36.128.157")
+    DB_PORT = os.getenv("DB_PORT", "3306")
+    DB_NAME = os.getenv("DB_NAME", "escolinha")
+
+    # Escapando os caracteres especiais na senha do banco de dados
+    DB_PASSWORD_ENC = quote_plus(DB_PASSWORD)
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD_ENC}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), "aplicacao_escolinha", "static", "uploads")
+
+    BASE_DIR = os.path.abspath(os.path.abspath(__file__))
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads', "atletas")
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from apliocacao_escolinha.extensions import db, login_manager
+from aplicacao_escolinha.extensions import db, login_manager
 from aplicacao_escolinha.models import Usuario
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
